2a1756adf697ad30ed6922f880d0ac51e02a47be
ahmad292005 <ahmadfathalla581@gmail.com>
2026-02-26 22:49:58 +0200

    react edit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./home.jsx";
+import Student from "./student.jsx";
+import "./App.css";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student" element={<Student />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
