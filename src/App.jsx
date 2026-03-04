4df9301d62ced869c8d4f2927a66fea74b545d93
alaa-emadd <ae159757@gmail.com>
2026-03-03 22:30:40 -0800

    edited app.jsx , add Routing ,DigitalCard ,CardStatus

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,23 @@ import Login from "./login.jsx";
 import Register from "./register.jsx";
 import Dashboard from "./Dashboard";
 import Classes from "./Classes";
+import DigitalCard from "./DigitalCard";
+import CardStatus from "./CardStatus";
+import DigitalIdPage from "./DigitalIdPage";
 
 import "./App.css";
 
 function App() {
+     const student = {
+    name: "name",
+    major: "Computer Science",
+    id: "2327",
+    photo:"" };
+
+     const studen = {
+    id: "12345",
+    lastScan: "2026-03-02 10:30 AM"
+  };
   return (
     <Router>
      <Routes>
@@ -15,7 +28,15 @@ function App() {
   <Route path="/register" element={<Register />} />
   <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/classes" element={<Classes />} />
-
+       <Route path="/digital-card" element={
+      <div style={{ padding: "2rem" }}>
+        <DigitalCard student={student} />
+      </div> } />
+     <Route path="/card-status" element={
+      <div style={{ padding: "2rem" }}>
+        <CardStatus student={studen} />
+      </div>} />
+    <Route path="/digital-id" element={<DigitalIdPage />} />
 </Routes>
     </Router>
   );
