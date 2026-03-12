af95f72681b902daafc15cfa59a60a19e445122f
mariaamhossam11-cpu <mariaamhossam11@gmail.com>
2026-03-12 22:40:19 +0200

    new attendance pages

---
+++ Security.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Security() {
+
+  return (
+    <div>
+
+      <h2>Security</h2>
+
+      <div>
+        <p>Change Password</p>
+        <input type="password" placeholder="New Password" />
+      </div>
+
+      <br/>
+
+      <button>Save</button>
+
+    </div>
+  );
+}
+
+export default Security;
